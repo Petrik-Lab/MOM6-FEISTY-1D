d6975f12344b15a0411c79465dd7a5b9cec39ce1
cpetrik <colleenpetrik@gmail.com>
2026-02-03 17:53:27 -0800

    added cobalt z outputs for all years

--- exps/OM4.single_column.COBALT/run_multiyear.sh
+++ exps/OM4.single_column.COBALT/run_multiyear.sh
@@ -276,7 +276,9 @@ do
     yes | cp -i *feisty*.nc "$YEAR_FOLDER_PATH"/
     yes | cp -i 20040101.ocean_cobalt_restart.nc "$YEAR_FOLDER_PATH"/
     yes | cp -i 20040101.ocean_cobalt_btm.nc "$YEAR_FOLDER_PATH"/
-    yes | cp -i 20040101.ocean_daily.nc "$YEAR_FOLDER_PATH"/
+    yes | cp -i 20040101.ocean_month_z.nc "$YEAR_FOLDER_PATH"/
+	yes | cp -i 20040101.ocean_cobalt_tracers_month_z.nc "$YEAR_FOLDER_PATH"/
+	yes | cp -i 20040101.ocean_cobalt_fluxes_int.nc "$YEAR_FOLDER_PATH"/
 
     # get restart files: 
     echo "Copying RESTART files back to INPUT, there is some clobbering!"
